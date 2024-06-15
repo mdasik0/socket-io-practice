809220a687c0d0ebcb454c29de69dce9de4bfda1
mdasik0 <asikthe1st@gmail.com>
2024-06-15 08:46:08 +0600

    leave room done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import JoinOrCreate from "./component/JoinOrCreate";
 
 const App = () => {
-  const [room, setRoom] = useState({
-    key: null,
-  });
-  if (room.key) {
-    return <div className="w-screen h-screen bg-yellow-50">
-      welcome to chat room
-    </div>;
+  const [room, setRoom] = useState(null);
+
+  useEffect(() => {
+    const currentlyJoined = JSON.parse(localStorage.getItem("Joined-room"));
+    console.log(currentlyJoined);
+    setRoom(currentlyJoined);
+  }, []);
+
+  const leaveRoom = () => {
+    localStorage.removeItem("Joined-room")
+    setRoom(null)
+  }
+
+  if (room) {
+    return (
+      <div className="w-screen h-screen bg-yellow-50 text-black">
+        <div className="flex justify-between items-center p-10">
+        <h1 className="text-3xl">
+          welcome to <span className="font-semibold">{room.roomName}</span>
+        </h1>
+        <button onClick={leaveRoom} className="px-4 py-2 bg-red-500 text-white hover:bg-red-400 duration-300 rounded">Leave room</button>
+        </div>
+      </div>
+    );
   } else {
     return (
       <>
-      <JoinOrCreate />
+        <JoinOrCreate />
       </>
     );
   }
